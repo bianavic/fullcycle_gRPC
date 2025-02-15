874497f8ad7200f3d243c98d4bc1a0572c01a485
bianavic <bianavic@gmail.com>
2025-02-15 14:23:32 -0300

    feat: implementa CreateCategory

---
+++ internal/service/category.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"github.com/bianavic/fullcycle_gRPC/internal/database"
+	"github.com/bianavic/fullcycle_gRPC/internal/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(db database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: db}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description) // in vem da request
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error creating category: %v", err)
+	}
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: *category.Description,
+	}
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, err
+}
