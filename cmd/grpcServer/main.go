89e186b871b5aa59d06a9dda551137c99936720b
bianavic <bianavic@gmail.com>
2025-02-15 14:29:16 -0300

    feat: create server gRPC

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/bianavic/fullcycle_gRPC/internal/database"
+	"github.com/bianavic/fullcycle_gRPC/internal/pb"
+	"github.com/bianavic/fullcycle_gRPC/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+	// le e processa a propria informacao
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+}
